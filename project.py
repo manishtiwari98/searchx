641363c9f586e8a8171ed15bb71416335c920dd4
manishtiwari98 <manishtiwari9700@gmail.com>
2017-07-12 22:11:14 +0530

    bash script added

--- project.py
+++ project.py
@@ -9,7 +9,6 @@ GREEN = '\033[92m'
 BOLD = '\033[1m'
 
 command =argv[1]
-#extracting first link from google
 
 def tgs(soup):
     for heading in soup.find_all(re.compile('^h[2-5]$')):
@@ -30,15 +29,18 @@ def tgs(soup):
         if tag.name==h_tag.name   :
             print(BOLD+tag.get_text())
         tag=tag.next_sibling
+
 def lifewire(link):
     r=requests.get(link)
     soup=bs(r.content,'html.parser')   
+    print(" "*20+BOLD+"Frequently used "+command+" commands"+"\n"*2)
     for block in soup.find_all('blockquote'):
         print(BOLD+block.find_previous_sibling('h3').string)
         print(GREEN+block.get_text())
+
+#extracting first 3 link from goole results
 g_url='https://www.google.com/search?q={}+command+examples&gbv=1&sei=YwHNVpHLOYiWmQHk3K24Cw'.format(command)
 g_page=requests.get(g_url)
-print(g_url)
 g_soup=bs(g_page.content,'html.parser')
 count=0
 while(count<4):
@@ -46,9 +48,6 @@ while(count<4):
     text=g_tag.get_text()
 
     link=g_tag.a['href'][7:g_tag.a['href'].index('&')]
-    print(link)
-    print(text)
-    print(count)
     if all(val in text.lower() for val in ['example', command]):
         if re.findall(r'(thegeekstuff|tecmint)',link.lower()):
             r=requests.get(link)
@@ -61,7 +60,5 @@ while(count<4):
         count+=1
     else:
         count+=1
-
-
-
-
+if count==4:
+    print(BOLD+'This command is not available')
